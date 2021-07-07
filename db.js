e7e60ee0765108a25a2a0ac59515456f11db0e96
mrenee73 <michelecountryman1@gmail.com>
2021-07-07 14:54:48 -0400

    fixing heroku and postman

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/HoptoIt`,{
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+
     ssl: process.env.ENVIRONMENT === 'production'
 })
 
